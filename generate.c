84cfdafacb8ce75d5b20bbaace95dd2ef7554ba7
Neha <neha182693@gmail.com>
2017-03-07 14:47:51 +0530

    Create generate.c

---
+++ generate.c
@@ -0,0 +1,55 @@
+/**
+ * generate.c
+ *
+ * Computer Science 50
+ * Problem Set 3
+ *
+ * Generates pseudorandom numbers in [0,LIMIT), one per line.
+ *
+ * Usage: generate n [s]
+ *
+ * where n is number of pseudorandom numbers to print
+ * and s is an optional seed
+ */
+ 
+#define _XOPEN_SOURCE
+
+#include <cs50.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+// constant
+#define LIMIT 65536
+
+int main(int argc, string argv[])
+{
+    // the number of command line argument is not equal to 2 or 3, then return 1
+    if (argc != 2 && argc != 3)
+    {
+        printf("Usage: generate n [s]\n");
+        return 1;
+    }
+
+    // changing the second command line argument type from string to integer
+    int n = atoi(argv[1]);
+
+    // if seed value is there, it will produce same set of random numbers else different set of random numbers because of time(NULL)
+    if (argc == 3)
+    {
+        srand48((long int) atoi(argv[2]));
+    }
+    else
+    {
+        srand48((long int) time(NULL));
+    }
+
+    // will print n random numbers
+    for (int i = 0; i < n; i++)
+    {
+        printf("%i\n", (int) (drand48() * LIMIT));
+    }
+
+    // success
+    return 0;
+}
